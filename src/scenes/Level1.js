08ec4f4a4c5fb05f8d9334d63978b42022e74c26
Andres Soto <84928376+asotito@users.noreply.github.com>
2025-05-30 16:09:25 -0500

    feat: modified level 1 to show images according to number of tries done by the user, and added images according to the try they are at.

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -1,3 +1,5 @@
+import { Classes } from '../shared/Classes.js'
+
 export class Level1 extends Phaser.Scene {
     constructor(){
         super('Level1');
@@ -5,24 +7,26 @@ export class Level1 extends Phaser.Scene {
 
     preload() {
         this.load.image('firtsImage', 'assets/napoleonBaby.png');
+        this.load.image('firstOpp', 'assets/level1Start.png');
+        this.load.image('firstError', 'assets/level1FirstError.png');
+        this.load.image('secondError', 'assets/level1SecondError.png');
+        this.load.image('thirdError', 'assets/level1ThirdError.png');
     }
 
     create() {
-        const napoleonBaby = this.add.image(0, 0, 'firtsImage').setOrigin(0).setInteractive()
+        this.classScale = new Classes(this);
 
-        const scaleNapoleonBabyX = this.cameras.main.width / napoleonBaby.width;
-        const scaleNapoleonBabyY = this.cameras.main.height / napoleonBaby.height;
+        const napoleonBaby = this.classScale.scaleImage(this.add.image(0, 0, 'firtsImage'), 1, 1, true, true, 0, 0);
+        const firstOpp = this.classScale.scaleImage(this.add.image(0, 0, 'firstOpp'), 1, 1, true, true, 0, 0);
+        const firstError = this.classScale.scaleImage(this.add.image(0, 0, 'firstError'), 1, 1, true, true, 0, 0);
+        const secondError = this.classScale.scaleImage(this.add.image(0, 0, 'secondError'), 1, 1, true, true, 0, 0);
+        const thirdError = this.classScale.scaleImage(this.add.image(0, 0, 'thirdError'), 1, 1, true, true, 0, 0);
 
-            // Use the smaller scale to maintain aspect ratio (fit inside)
-        const napoleonBabyScale = Math.min(scaleNapoleonBabyX, scaleNapoleonBabyY);
+        firstOpp.alpha = 0;
+        firstError.alpha = 0;
+        secondError.alpha = 0;
+        thirdError.alpha = 0;
 
-        napoleonBaby.setScale(napoleonBabyScale);
-
-        // Optionally center it if you want it centered
-        napoleonBaby.x = (this.cameras.main.width - napoleonBaby.displayWidth) / 2;
-        napoleonBaby.y = (this.cameras.main.height - napoleonBaby.displayHeight) / 2;
-
-        
         napoleonBaby.on('pointerdown', () => {
             this.tweens.add({
                 targets: napoleonBaby,
@@ -32,8 +36,65 @@ export class Level1 extends Phaser.Scene {
                     napoleonBaby.destroy(); // eliminar después del fade out
                 }
             });
+            this.tweens.add({
+                targets: firstOpp,
+                alpha: 1,
+                duration: 500,
+            });
         });
-        
-        
+
+        this.contador = 0;
+        this.showedImage = firstOpp;
+
+        this.input.keyboard.on('keydown', (event) => {
+            if(event.key === '1'){
+                this.tweens.add({
+                    targets: this.showedImage,
+                    alpha: 0,
+                    duration: 500,
+                    onComplete: () => {
+                        this.showedImage.destroy(); // eliminar después del fade out
+                }
+            });
+            } else {
+                this.contador++;
+                console.log();
+                switch (this.contador) {
+                    case 1:
+                        this.showedImage = firstError;
+                        this.tweens.add({
+                            targets: this.showedImage,
+                            alpha: 1,
+                            duration: 500,
+                            onComplete: () => {
+                                firstOpp.destroy();
+                            }
+                        });
+                        break;
+                    case 2:
+                        this.showedImage = secondError;
+                        this.tweens.add({
+                            targets: this.showedImage,
+                            alpha: 1,
+                            duration: 500,
+                            onComplete: () => {
+                                firstError.destroy();
+                            }
+                        });
+                        break;
+                    case 3:
+                        this.showedImage = thirdError;
+                        this.tweens.add({
+                            targets: this.showedImage,
+                            alpha: 1,
+                            duration: 500,
+                            onComplete: () => {
+                                secondError.destroy();
+                            }
+                        });
+                        break;
+                    }
+            }
+        });
     }
 }
